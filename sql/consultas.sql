ca26ffa27b01959a279785663ce09d5f1d589366
Franco Manuel Lonello Serra <francolonelloserra@gmail.com>
2026-06-04 18:27:16 -0300

    corrijo consultas usando SUM y GROUP BY

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -1,19 +1,21 @@
 USE liga_argentina_2026;
 
 -- Top 10 goleadores
-SELECT j.nombre, e.nombre AS equipo, s.goles
+SELECT j.nombre, e.nombre AS equipo, SUM(s.goles) AS total_goles
 FROM jugadores j
 JOIN equipos e ON j.id_equipo = e.id_equipo
 JOIN estadisticas s ON j.id_jugador = s.id_jugador
-ORDER BY s.goles DESC
+GROUP BY j.nombre, e.nombre
+ORDER BY total_goles DESC
 LIMIT 10;
 
 -- Top 10 asistencias
-SELECT j.nombre, e.nombre AS equipo, s.asistencias
+SELECT j.nombre, e.nombre AS equipo, SUM(s.asistencias) AS total_asistencias
 FROM jugadores j
 JOIN equipos e ON j.id_equipo = e.id_equipo
 JOIN estadisticas s ON j.id_jugador = s.id_jugador
-ORDER BY s.asistencias DESC
+GROUP BY j.nombre, e.nombre
+ORDER BY total_asistencias DESC
 LIMIT 10;
 
 -- Jugadores con más amarillas
